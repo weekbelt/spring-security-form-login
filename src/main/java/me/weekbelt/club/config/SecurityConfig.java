d79d1d857d7488a55eb5ea408c8def59efcec1ee
weekbelt <vfrvfr4207@gmail.com>
2021-02-10 01:27:31 +0900

    feat: Config PasswordEncoder

--- src/main/java/me/weekbelt/club/config/SecurityConfig.java
+++ src/main/java/me/weekbelt/club/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package me.weekbelt.club.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
---
+++ src/test/java/me/weekbelt/club/PasswordTests.java
@@ -0,0 +1,25 @@
+package me.weekbelt.club;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordTests {
+
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    @DisplayName("패스워드인코더 테스트")
+    public void testcase() {
+        String password = "1111";
+        String enPw = passwordEncoder.encode(password);
+        System.out.println("enPw: " + enPw);
+        boolean matchResult = passwordEncoder.matches(password, enPw);
+        System.out.println("matchResult: " + matchResult);
+    }
+}
