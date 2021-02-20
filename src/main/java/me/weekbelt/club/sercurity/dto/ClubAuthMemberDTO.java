e1c4d9e64a73ee388366ff1f1f9e1bd8ed91bef0
weekbelt <vfrvfr4207@gmail.com>
2021-02-20 20:02:04 +0900

    feat: Add User dto

---
+++ src/main/java/me/weekbelt/club/sercurity/dto/ClubAuthMemberDTO.java
@@ -0,0 +1,29 @@
+package me.weekbelt.club.sercurity.dto;
+
+import java.util.Collection;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+@Slf4j
+@Setter
+@Getter
+@ToString
+public class ClubAuthMemberDTO extends User {
+
+    private String email;
+
+    private String name;
+
+    private boolean fromSocial;
+
+    public ClubAuthMemberDTO(String username, String password, boolean fromSocial, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.email = username;
+        this.fromSocial = fromSocial;
+    }
+
+}
