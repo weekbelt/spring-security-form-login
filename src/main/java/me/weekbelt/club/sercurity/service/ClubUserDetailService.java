1dab3eca0f57f7c6b18bef60bfa25298e0592524
weekbelt <vfrvfr4207@gmail.com>
2021-02-21 22:30:01 +0900

    feat: Create ClubUserDetailService

---
+++ src/main/java/me/weekbelt/club/sercurity/service/ClubUserDetailService.java
@@ -0,0 +1,18 @@
+package me.weekbelt.club.sercurity.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ClubUserDetailService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("ClubUserDetailService loadUserByUsername " + username);
+        return null;
+    }
+}
