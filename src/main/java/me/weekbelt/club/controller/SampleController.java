c2d479a84d2e90493cc5ede4c8f0705712756de0
weekbelt <vfrvfr4207@gmail.com>
2021-02-23 23:05:07 +0900

    feat: Confirm authentication

--- src/main/java/me/weekbelt/club/controller/SampleController.java
+++ src/main/java/me/weekbelt/club/controller/SampleController.java
@@ -1,6 +1,8 @@
 package me.weekbelt.club.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import me.weekbelt.club.sercurity.dto.ClubAuthMemberDTO;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,12 @@ public class SampleController {
     }
 
     @GetMapping("/member")
-    public void exMember() {
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
+
         log.info("exMember.............");
+
+        log.info("---------------------------");
+        log.info("{}", clubAuthMemberDTO);
     }
 
     @GetMapping("/admin")
