dc3179d49e54553880857af52de73e9b0f32970d
weekbelt <vfrvfr4207@gmail.com>
2021-02-24 21:05:10 +0900

    feat: Config google OAuth2 API

--- src/main/java/me/weekbelt/club/config/SecurityConfig.java
+++ src/main/java/me/weekbelt/club/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin();
         http.csrf().disable();
-        http.logout();
+
+        http.oauth2Login();
     }
 }
