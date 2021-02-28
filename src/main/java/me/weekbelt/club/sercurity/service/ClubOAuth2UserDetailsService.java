d3a4db9ad222e6a9f8fbe9c6df392757f3da67a5
weekbelt <vfrvfr4207@gmail.com>
2021-02-28 14:51:06 +0900

    feat: save google account info

--- src/main/java/me/weekbelt/club/sercurity/service/ClubOAuth2UserDetailsService.java
+++ src/main/java/me/weekbelt/club/sercurity/service/ClubOAuth2UserDetailsService.java
@@ -1,6 +1,12 @@
 package me.weekbelt.club.sercurity.service;
 
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.weekbelt.club.entity.ClubMember;
+import me.weekbelt.club.entity.ClubMemberRole;
+import me.weekbelt.club.repository.ClubMemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -9,7 +15,12 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ClubOAuth2UserDetailsService extends DefaultOAuth2UserService {
+
+    private final ClubMemberRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -27,6 +38,32 @@ public class ClubOAuth2UserDetailsService extends DefaultOAuth2UserService {
         oAuth2User.getAttributes().forEach((k, v) -> {
             log.info(k + ":" + v);
         });
+
+        String email = null;
+        if (clientName.equals("Google")) {
+            email = oAuth2User.getAttribute("email");
+        }
+
+        ClubMember clubMember = saveSocialMember(email);
+
         return oAuth2User;
+    }
+
+    private ClubMember saveSocialMember(String email) {
+        final Optional<ClubMember> result = repository.findByEmail(email, true);
+        if (result.isPresent()) {
+            return result.get();
+        }
+
+        final ClubMember clubMember = ClubMember.builder()
+            .name(email)
+            .password(passwordEncoder.encode("1111"))
+            .fromSocial(true)
+            .build();
+
+        clubMember.addMemberRole(ClubMemberRole.USER);
+        repository.save(clubMember);
+
+        return clubMember;
     }
 }
